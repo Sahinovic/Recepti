c617818f6e29b4387702837e5f83579e78558d70
Mirza <sahinovic@gmail.com>
2020-12-17 23:12:13 +0100

    aded custom user model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class MoodelTests(TestCase):
+
+    def test_create_user_with_email_successfull(self):
+        """test creating a new user with an email is successful"""
+        email="test@test.com"
+        password = "test1234"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password= password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """test the email for a new user is ormalized"""
+        email = 'test@TEST.COM'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises eror"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_super_user(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@test123.com',
+            'test123')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
