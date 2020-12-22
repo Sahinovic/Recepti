1f8ab64b6fdbb99d9545eb6725d4de788ab69ce3
Mirza <sahinovic@gmail.com>
2020-12-22 22:52:44 +0100

    aded manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -13,6 +13,15 @@ class UserSerializer(serializers.ModelSerializer):
         """create new user with enctipted pass and return it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """update a user setting the pass corectly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
